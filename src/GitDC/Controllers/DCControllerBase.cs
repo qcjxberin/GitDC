919a262e3b104eabe059bad6cc3bb015962262ca
丁川 <100538511@qq.com>
2019-08-04 21:03:45 +0800

    ok

---
+++ src/GitDC/Controllers/DCControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ding.Webs.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitDC.Controllers
+{
+    public class DCControllerBase : WebControllerBase
+    {
+
+    }
+}
